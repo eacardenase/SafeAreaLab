64da4ec31494eb6c4300ab465c686211309ccc9c
Edwin Cardenas <eacardenase@unal.edu.co>
2023-03-23 11:43:30 -0500

    refactor: no storyboard setup

--- SafeAreaLab/ViewController.swift
+++ SafeAreaLab/ViewController.swift
@@ -11,9 +11,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
-
 }
 
+extension ViewController {
+    func setupViews() {
+        let toplabel = makeLabel(withText: "top")
+        let bottomLabel = makeLabel(withText: "bottom")
+        let leadingLabel = makeLabel(withText: "leading")
+        let trailingLabel = makeLabel(withText: "trailing")
+        
+        view.addSubview(toplabel)
+        view.addSubview(bottomLabel)
+        view.addSubview(leadingLabel)
+        view.addSubview(trailingLabel)
+        
+        // topLabel
+        NSLayoutConstraint.activate([
+            toplabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            toplabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        // bottomLabel
+        NSLayoutConstraint.activate([
+            bottomLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -8),
+            bottomLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        // leadingLabel
+        NSLayoutConstraint.activate([
+            leadingLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            leadingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        // trailingLabel
+        NSLayoutConstraint.activate([
+            trailingLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -8),
+            trailingLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    func makeLabel(withText text: String) -> UILabel {
+        let label = UILabel()
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = text
+        label.backgroundColor = .yellow
+        label.font = UIFont.systemFont(ofSize: 32)
+        
+        return label
+    }
+}
